69675732df0d43123f212260efb03ea9b7dd2970
Matthew Finlayson <matthew.finlayson@jivesoftware.com>
2014-01-17 17:30:53 -0800

    REWRITE FOR JUSTICE

--- views/blog.py
+++ views/blog.py
@@ -24,3 +24,12 @@ def fancy(year, month, day, title):
     #print links
     #return render_template("article.html", title=article["title"], item=article, links=links)
     return render_template("article.html", title=article["title"], item=article)
+
+@blog.route('/<year>/<month>/<day>/<title>/2')
+def fancy2(year, month, day, title):
+    new_url = "%s/%s/%s/%s/" % (year, month, day, title)
+    article = a.fetch_one(new_url)
+    #links = keywords.get_links(article["tags"].split())
+    #print links
+    #return render_template("article.html", title=article["title"], item=article, links=links)
+    return render_template("article2.html", title=article["title"], item=article)
