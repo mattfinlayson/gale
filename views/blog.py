ea41599561581749dc79ad882584533e519a4f3e
Matthew Finlayson <matthew.finlayson@jivesoftware.com>
2014-01-20 13:00:52 -0800

    More cleanup

--- views/blog.py
+++ views/blog.py
@@ -11,8 +11,6 @@ a = ArticleProvider()
 
 @blog.route('/')
 def index():
-    for key, value in a.latest().iteritems():
-        print key, value
     return render_template("index.html", title="index", items=a.parsed_list)
 
 
@@ -25,11 +23,6 @@ def fancy(year, month, day, title):
     #return render_template("article.html", title=article["title"], item=article, links=links)
     return render_template("article.html", title=article["title"], item=article)
 
-@blog.route('/<year>/<month>/<day>/<title>/2')
-def fancy2(year, month, day, title):
-    new_url = "%s/%s/%s/%s/" % (year, month, day, title)
-    article = a.fetch_one(new_url)
-    #links = keywords.get_links(article["tags"].split())
-    #print links
-    #return render_template("article.html", title=article["title"], item=article, links=links)
-    return render_template("article2.html", title=article["title"], item=article)
+@blog.route('/feed')
+def feed():
+    return render_template("index.xml", title="index", items=a.parsed_list)
